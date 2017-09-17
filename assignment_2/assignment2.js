4c692be9c8f78d18ec6897d95ae4365b86cdc84f
Andrew Maranta <andrew@maranta.ca>
2017-09-17 19:41:16 -0400

    Assignment 2

--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -3,6 +3,10 @@ Exercise #1
 Create a for loop that prints out the numbers 1 to 100 in the console.
 */
 
+for (var i=1;i<=100; i++){
+    console.log(i);
+}
+
 
 
 /*
@@ -17,3 +21,12 @@ Write a loop that makes seven calls to console.log to output the following trian
 ######
 #######
 */
+
+var x='#';
+
+for(var i=0;i<7;i++){
+    console.log(x); 
+    if(i<x.length){
+    x+='#';
+    }
+}
